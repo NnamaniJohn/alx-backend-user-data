34ff0bd361a8c5815ed9a6fc95049757bd93b430
Nnamani John <nnamanijohn123@gmail.com>
2024-04-16 16:16:36 +0100

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,13 +9,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication."""
-        if path is None or excluded_paths is None or excluded_paths == []:
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path[-1] != '/':
-            path += '/'
-        if path not in excluded_paths:
-            return True
-        return False
+        for p in excluded_paths:
+            if p.endswith('*') and path.startswith(p[:-1]):
+                return False
+            if path == p:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header."""
