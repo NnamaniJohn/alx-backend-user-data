f7ec4b01be021badf79dcfce3d5edce102f57794
Nnamani John <nnamanijohn123@gmail.com>
2024-04-16 01:17:33 +0100

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class."""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract base64 authorization header."""
+        if (authorization_header is None or not
+                isinstance(authorization_header, str)):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
